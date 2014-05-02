44674e309c33cee609b2062f05a9e3c0915c0f86
Luca de Alfaro <luca@dealfaro.org>
2014-05-01 21:56:04 -0700

    Voila, all fixed.

--- src/com/dealfaro/android/doinbackground/SendActivity.java
+++ src/com/dealfaro/android/doinbackground/SendActivity.java
@@ -43,7 +43,7 @@ public class SendActivity extends Activity {
 	String myId;
 	
 	// Background downloader.
-	private ServerConnect downloader = null;
+	private ServerCall downloader = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -74,30 +74,17 @@ public class SendActivity extends Activity {
 		m.put("secret", MainActivity.MY_SECRET);
 		m.put("id", myId);
 		m.put("username", username);
-		spec.params = m;
+		spec.setParams(m);
 		spec.activity = getApplication();
 		// Initiates server call.
-		downloader = new ServerConnect();
+		downloader = new ServerCall();
 		downloader.execute(spec);
 		
 	}
 	
 	
-	// Specification for accessing the server.
-	class Spec {
-		Spec() {};
-		
-		public String url;
-		public Application activity;
-		public HashMap<String, String> params;
-		public void useResult(Application context, String r) {
-			Log.i(LOG_TAG, "Result: " + r);
-		}
-	}
-
-	
 	// Checking for username uniqueness.
-	class SendSpec extends Spec {
+	class SendSpec extends ServerCallSpec {
 		@Override
 		public void useResult(Application context, String r) {
 			// If we get a null result, the server is down.
@@ -120,128 +107,4 @@ public class SendActivity extends Activity {
 		}
 	}
 	
-	class WhoIsHereSpec extends Spec {
-		@Override
-		public void useResult(Application activity, String r) {
-			// Here goes the Json decoding etc.
-			Log.i(LOG_TAG, "Result to use: " + r);			
-		}
-	}
-	
-	class PostProcessPair {
-		PostProcessPair() {};
-		public Spec spec;
-		public String result;
-	}
-	
-		
-    // This class downloads from the net the camera setup instructions.
-    private class ServerConnect extends AsyncTask<Spec, String, PostProcessPair> {
-    	
-    	protected PostProcessPair doInBackground(Spec... specs) {
-    		Log.d(LOG_TAG, "Starting the download.");
-    		String downloadedString = null;
-    		Spec spec = specs[0];
-    		URI url = URI.create(spec.url);
-    		int numTries = 0;
-    		while (downloadedString == null && numTries < MainActivity.MAX_SETUP_DOWNLOAD_TRIES && !isCancelled()) {
-    			numTries++;
-    			HttpPost request = new HttpPost(url);
-    			// We need to add the parameters.
-    			UrlEncodedFormEntity form = urlEncodeHashMap(spec.params);
-    			request.setEntity(form);
-    	
-    			DefaultHttpClient httpClient = new DefaultHttpClient();
-    			HttpResponse response = null;
-    			try {
-    				response = httpClient.execute(request);
-    			} catch (ClientProtocolException ex) {
-    				Log.e(LOG_TAG, ex.toString());
-    			} catch (IOException ex) {
-    				Log.e(LOG_TAG, ex.toString());
-    			}
-    			if (response != null) {
-    				// Checks the status code.
-    				int statusCode = response.getStatusLine().getStatusCode();
-    				Log.d(LOG_TAG, "Status code: " + statusCode);
-
-    				if (statusCode == HttpURLConnection.HTTP_OK) {
-    					// Correct response. Reads the real result.
-    					// Extracts the string content of the response.
-    					HttpEntity entity = response.getEntity();
-    					InputStream iStream = null;
-    					try {
-    						iStream = entity.getContent();
-    					} catch (IOException ex) {
-    						Log.e(LOG_TAG, ex.toString());
-    					}
-    					if (iStream != null) {
-    						downloadedString = ConvertStreamToString(iStream);
-    						Log.d(LOG_TAG, "Received string: " + downloadedString);
-    			    		// Postprocess the result.
-    			    		PostProcessPair instr = new PostProcessPair();
-    			    		instr.spec = spec;
-    			    		instr.result = downloadedString;
-    			    		return instr;
-    					}
-    				}
-    			}
-    		}
-    		// Postprocess the result.
-    		PostProcessPair instr = new PostProcessPair();
-    		instr.spec = spec;
-    		instr.result = downloadedString;
-    		return instr;
-    	}
-    	
-    	protected void onPostExecute(PostProcessPair instr) {
-    		// This is executed in the UI thread.
-    		instr.spec.useResult(instr.spec.activity, instr.result);
-    	}
-    }
-    
-    public static UrlEncodedFormEntity urlEncodeHashMap(HashMap<String, String> params) {
-    	List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-    	Iterator<Map.Entry<String, String>> it = params.entrySet().iterator();
-    	while (it.hasNext()) {
-    		Map.Entry<String, String> pair = it.next();
-    		nameValuePairs.add(new BasicNameValuePair(pair.getKey(), pair.getValue()));
-    	}
-    	UrlEncodedFormEntity form;
-    	try {
-    		form = new UrlEncodedFormEntity(nameValuePairs, "UTF-8");
-    	} catch (UnsupportedEncodingException e) {
-    		Log.e(LOG_TAG, "Encoding exception: " + e.toString());
-    		return null;
-    	}
-    	return form;
-    }
-
-    public static String ConvertStreamToString(InputStream is) {
-    	
-    	if (is == null) {
-    		return null;
-    	}
-    	
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-	    StringBuilder sb = new StringBuilder();
-
-	    String line = null;
-	    try {
-	        while ((line = reader.readLine()) != null) {
-	            sb.append((line + "\n"));
-	        }
-	    } catch (IOException e) {
-	        Log.d(LOG_TAG, e.toString());
-	    } finally {
-	        try {
-	            is.close();
-	        } catch (IOException e) {
-	            Log.d(LOG_TAG, e.toString());
-	        }
-	    }
-	    return sb.toString();
-	}
-
-
 }
